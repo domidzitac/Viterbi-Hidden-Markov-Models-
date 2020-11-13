2f0ff581e081e66eeb268de8011a0e1df397a096
Domnica Dzitac <domnica.dzitac@nyu.edu>
2020-11-13 13:16:03 +0400

    Add files via upload

---
+++ did233_HW3 2/WSJ_POS_CORPUS_FOR_STUDENTS/score.py
@@ -0,0 +1,71 @@
+#!/usr/bin/python
+#
+# scorer for NLP class Spring 2016
+# ver.1.0
+#
+# score a key file against a response file
+# both should consist of lines of the form:   token \t tag
+# sentences are separated by empty lines
+#
+import sys
+
+def score (keyFileName, responseFileName):
+    keyFile = open(keyFileName, 'r')
+    key = keyFile.readlines()
+    responseFile = open(responseFileName, 'r')
+    response = responseFile.readlines()
+
+    if len(key) != len(response):
+        print("length mismatch between key and submitted file")
+        exit(-1)
+
+    correct = 0
+    incorrect = 0
+
+    for i in range(len(key)):
+        key[i] = key[i].rstrip('\n')
+        response[i] = response[i].rstrip('\n')
+
+        if key[i] == "":
+            if response[i] == "":
+                continue
+            else:
+                print("sentence break expected at line " + str(i))
+                exit(-1)
+
+        keyFields = key[i].split('\t')
+
+        if len(keyFields) != 2:
+            print("format error in key at line " + str(i) + ":" + key[i])
+            exit(-1)
+
+        keyToken = keyFields[0]
+        keyPos = keyFields[1]
+        responseFields = response[i].split('\t')
+
+        if len(responseFields) != 2:
+            print("format error at line " + str(i))
+            exit(-1)
+
+        responseToken = responseFields[0]
+        responsePos = responseFields[1]
+
+        if responseToken != keyToken:
+            print("token mismatch at line " + str(i))
+            exit(-1)
+
+        if responsePos == keyPos:
+            correct = correct + 1
+        else:
+            incorrect = incorrect + 1
+
+    print(str(correct) + " out of " + str(correct + incorrect) + " tags correct")
+    accuracy = 100.0 * correct / (correct + incorrect)
+    print("  accuracy: {0}".format(accuracy))
+
+def main(args):
+	key_file = args[1]
+	response_file = args[2]
+	score(key_file,response_file)
+
+if __name__ == '__main__': sys.exit(main(sys.argv))
